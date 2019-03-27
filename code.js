a78421f54b297b2ce5a7ef34244ed85224fb5cf2
Jeff Sharer <jefflim076@gmail.com>
2019-03-27 19:15:58 +0700

    First Commit

---
+++ code.js
@@ -0,0 +1,28 @@
+onEvent("letsGetStartedButton", "click", function( ) {
+  setScreen("MainGiftCardDesignSelectionScreen");
+});
+onEvent("goToSurveyButton", "click", function( ) {
+  setScreen("SurveyScreen");
+});
+onEvent("resubmitFormButton", "click", function( ) {
+  setScreen("SurveyScreen");
+});
+onEvent("goBackToGiftCardSelectionPageButton", "click", function( ) {
+  setScreen("MainGiftCardDesignSelectionScreen");
+});
+onEvent("submitFormButton", "click", function( ) {
+  createRecord("FormSubmissionTable", {name:(getText("nameTextField")), Feelings:(getText("feelingsDropDownList")), review:(getText("thougthsAndOpinionsTextField"))}, function(record) {
+    
+  });
+  setScreen("ThankYouForSubmission!");
+});
+onEvent("happyBirthdayCardButton", "click", function( ) {
+  setScreen("BirthdayGiftCardEditorScreen");
+});
+onEvent("createButton", "click", function( ) {
+  setScreen("ResultOfCardScreen");
+  setText("whoMadeTheCard", "By " + getText("yourNameTextField"));
+  setText("nameOfThePersonHavingTheBirthday", getText("nameOfPersonThatHasTheBirthdayOrEvent"));
+  setText("ageOfTheOtherPerson", getText("ageOfTheOtherPerson"));
+  setText("aLittleQuoteTheyGave", getText("tryGivingAQuoteTextField"));
+});
